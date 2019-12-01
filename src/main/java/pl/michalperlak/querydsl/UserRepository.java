06849307912299bcaa3ed7da3e2d11081e784dc0
Michał Perlak <michalperlak@windowslive.com>
2019-12-01 23:22:01 +0100

    Example of using QueryDSL projections with Spring Data

--- src/main/java/pl/michalperlak/querydsl/UserRepository.java
+++ src/main/java/pl/michalperlak/querydsl/UserRepository.java
@@ -3,8 +3,6 @@ package pl.michalperlak.querydsl;
 
 import org.springframework.data.jpa.querydsl.ExtendedQuerydslPredicateExecutor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 interface UserRepository extends JpaRepository<User, Long>, ExtendedQuerydslPredicateExecutor<User> {
 }
